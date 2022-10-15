e0b1f958fa4e7a234543fce22ce409b62d076aae
Sarah Serussi <sarah.serussi@gmail.com>
2022-10-15 14:00:13 +0200

    use typescript and add new logo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./duck.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Dukkling</code>
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
